613f67b748aa9627ada0fe46beca7c4a2480b27b
pattyyepez <150443257+pattyyepez@users.noreply.github.com>
2025-04-05 22:15:01 +0200

    feat: entities, DbContext and data access layer

--- Backend/Entities/Department.cs
+++ Backend/Entities/Department.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Entities;
 
 public class Department
 {
-    
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    // Navigation Property
+    public ICollection<Story> Stories { get; set; }
 }
